0a9a0fa22504768575f1ff5fc3541e372851c4f0
MichalKinas <113341662+MichalKinas@users.noreply.github.com>
2025-08-04 09:59:20 +0200

    [ACS-9962] Add ACS template generator (#4)

--- tools/adf-generators/src/generators/shared/common-files-schema.ts
+++ tools/adf-generators/src/generators/shared/common-files-schema.ts
@@ -1,4 +1,7 @@
 export interface CommonFilesSchema {
   projectName: string;
   projectRoot: string;
+  template: 'empty' | 'acs' | 'aps' | 'acs-aps';
+  authType: 'BASIC' | 'OAUTH';
+  provider: 'ECM' | 'BPM' | 'ALL';
 }
