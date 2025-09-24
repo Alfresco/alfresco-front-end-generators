f24654cc9dae182ddba644b9d3a170f35098cad5
MichalKinas <michal.kinas@hyland.com>
2025-09-24 13:14:34 +0200

    [ACS-10190]  Final dependencies bump and paths cleanup

--- tools/adf-generators/jest.config.ts
+++ tools/adf-generators/jest.config.ts
@@ -10,7 +10,7 @@ const swcJestConfig = JSON.parse(
 swcJestConfig.swcrc = false;
 
 export default {
-  displayName: '@alfresco-front-end-generators/adf-generators',
+  displayName: '@alfresco/adf-generators',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
