d3029f9d50ecb301db711a1a954a74d54cec6e99
MichalKinas <113341662+MichalKinas@users.noreply.github.com>
2025-10-08 16:10:09 +0200

    [ACS-10445] Add APA and ACS-APA templates (#18)

--- tools/adf-generators/src/generators/template-app/schema.d.ts
+++ tools/adf-generators/src/generators/template-app/schema.d.ts
@@ -1,6 +1,6 @@
 export interface TemplateAppGeneratorSchema {
   name: string;
-  template: 'empty' | 'acs' | 'aps' | 'acs-aps';
+  template: 'empty' | 'acs' | 'aps' | 'acs-aps' | 'apa' | 'acs-apa';
   authType?: 'BASIC' | 'OAUTH';
   provider?: 'ECM' | 'BPM' | 'ALL';
 }
