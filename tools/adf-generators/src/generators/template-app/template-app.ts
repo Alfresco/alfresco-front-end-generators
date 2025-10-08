d86d395395d6cf95383c30cddc1498357b0f6589
MichalKinas <michal.kinas@hyland.com>
2025-10-08 16:07:28 +0200

    [ACS-10445] Add APA and ACS-APA templates

--- tools/adf-generators/src/generators/template-app/template-app.ts
+++ tools/adf-generators/src/generators/template-app/template-app.ts
@@ -41,9 +41,39 @@ export async function templateAppGenerator(
     }
   }
 
-  if (options.template === 'aps') {
+  if (options.template === 'aps' || options.template === 'apa') {
     if (tree.exists(`${projectRoot}/src/app/documents`)) {
       tree.delete(`${projectRoot}/src/app/documents`);
+    }
+  }
+
+  if (options.template === 'apa' || options.template === 'acs-apa') {
+    if (tree.exists(`${projectRoot}/src/app/apps`)) {
+      tree.delete(`${projectRoot}/src/app/apps`);
+    }
+    if (tree.exists(`${projectRoot}/src/app/start-process`)) {
+      tree.delete(`${projectRoot}/src/app/start-process`);
+    }
+    if (tree.exists(`${projectRoot}/src/app/task-details`)) {
+      tree.delete(`${projectRoot}/src/app/task-details`);
+    }
+    if (tree.exists(`${projectRoot}/src/app/tasks`)) {
+      tree.delete(`${projectRoot}/src/app/tasks`);
+    }
+  }
+
+  if (options.template !== 'apa' && options.template !== 'acs-apa') {
+    if (tree.exists(`${projectRoot}/src/app/apps-cloud`)) {
+      tree.delete(`${projectRoot}/src/app/apps-cloud`);
+    }
+    if (tree.exists(`${projectRoot}/src/app/start-process-cloud`)) {
+      tree.delete(`${projectRoot}/src/app/start-process-cloud`);
+    }
+    if (tree.exists(`${projectRoot}/src/app/task-details-cloud`)) {
+      tree.delete(`${projectRoot}/src/app/task-details-cloud`);
+    }
+    if (tree.exists(`${projectRoot}/src/app/tasks-cloud`)) {
+      tree.delete(`${projectRoot}/src/app/tasks-cloud`);
     }
   }
 
